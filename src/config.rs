8860bf3df1fc515e3e39955f804a780bdd1d45bb
Mr.Lin <lsx0044@gmail.com>
2025-05-28 18:37:02 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["hy-aliyun.rs.ldxw.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["hy.rs.ldxw.top"];
 pub const RS_PUB_KEY: &str = "icfwBZitk2jpRE6mgV7uthKpDp9HLECf+GfGVyXsFi8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
